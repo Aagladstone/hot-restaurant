306863f4d154dbd7d77e5b361000b85d2352de44
Aaron <aarongladstone456@gmail.com>
2019-03-05 20:16:46 -0600

    added routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependency
 var express = require('express');
 var app = express();
+var path = require("path");
 
 // Set PORT to 3000 if not specified by Heroku
 var PORT = process.env.PORT || 3000;
@@ -15,6 +16,18 @@ app.get('/api/tables', function(req, res){
 // Create set of routes that display waitlist data as JSONs
 app.get('/api/waitlist', function(req, res){
     res.json(waitlist); // sends content from waitlist to URL as JSON
+});
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reservation", function(req, res){
+    res.sendFile(path.join(__dirname, "reservations.html"))
+});
+
+app.get("/viewtables", function(req, res){
+    res.sendFile(path.join(__dirname, "tables.html"))
 });
 
 // Connect server to PORT

bb4e70bebec920001d6586281ed90de42635df81
Dara Anya <daraanya@gmail.com>
2019-03-05 21:22:14 -0600

    updated server

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 // Dependency
 var express = require('express');
-var app = express();
 var path = require("path");
+
+var app = express();
+
+// This will help express understand the JSON we need in order to request/post information from the form and push into the arrays
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 // Set PORT to 3000 if not specified by Heroku
 var PORT = process.env.PORT || 3000;
@@ -29,6 +34,25 @@ app.get("/reservation", function(req, res){
 app.get("/viewtables", function(req, res){
     res.sendFile(path.join(__dirname, "tables.html"))
 });
+
+// Push information from form to the tables or waitlist array
+// Getting JSON object from form 
+app.post("/api/reservation", function(req, res){ // NOTE: request, is to receive data. res , is to send data
+    // Create a variable to request/store data from the form
+    var reservation = req.body;
+
+    // There are only 5 tables therefore we check if there are any tables avaliable and then push the data to the appropriate array
+    if (tables.length < 5){
+        tables.push(reservation);
+        res.json({reservation: true}); // This creates an object with the property of reservation. Reservation has the value true or false depending on table availibility 
+        console.log(reservation);
+    }
+    else{
+        waitlist.push(reservation);
+        res.json({reservation: false});
+    }
+});
+
 
 // Connect server to PORT
 app.listen(PORT, function(){
